e1164059831bf51da986b93e358e964b5a6d77b5
Bert154-beep <masabayan15@gmail.com>
2026-04-18 23:46:53 +0500

    Backend-Architecture

---
+++ backend/Server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+app.listen(3000, ()=> {
+    console.log("Server Is Running At PORT")
+})
---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const { drizzle } = require("drizzle-orm/node-postgres")
+const { Pool } = require("pg")
+const schema = require("../models/schema")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+})
+
+const db = drizzle(pool, { schema })
+
+module.exports = { db }
