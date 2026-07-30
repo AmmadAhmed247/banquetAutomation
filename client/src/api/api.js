f19cd18a19b55d6177eb4ed19cee48bdfcf8e7bb
Ammad Ahmed <ammadwork123@gmail.com>
2026-07-31 02:59:46 +0500

    --

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "https://banquetautomation.onrender.com",
+    baseURL: "https://raabta.tech",
     withCredentials: true,
 })      
